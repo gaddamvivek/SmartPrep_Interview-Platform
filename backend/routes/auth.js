bd69f2eb877b1cb7ae1ec922c94d837dd3797fa4
Aakanksha-Rangdal <arrangdal@gmail.com>
2024-10-23 10:57:30 -0400

    Bug Fix: saving interview details in the database upon form submission

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user'); // Import the User model
-const IDS  = require('../models/intrwdtlsschema')
+const IDSchema  = require('../models/intrwdtlsschema')
 const router = express.Router();
 const admin = require('../firebaseAdmin');
 const sessionTable = require('../models/sessionTable');
@@ -73,12 +73,10 @@ router.post('/interviewdetails', async (req, res) => {
                     throw new Error("Secret key not found");
                 }
                 const fetchedDetails = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
-                username = fetchedDetails.username;
+                username = fetchedDetails.mailid;
                 console.log("User mail ID:", username);
 
                 // Send a response and return to avoid further code execution
-                res.send('success');
-                return;
             } catch (error) {
                 console.log(error);
                 return res.status(401).json({ message: "Token verification failed" }); // Send error response and stop execution
@@ -89,8 +87,16 @@ router.post('/interviewdetails', async (req, res) => {
         }
 
         // If the token was valid, save interview details
-        const ids = new IDS({ username, prepname, diffLvl, slctround });
+        const ids = new IDSchema({ username, prepname, diffLvl, slctround });
         await ids.save();
+        const savedDetails = await IDSchema.findOne({ username, prepname, diffLvl, slctround });
+        if (savedDetails) {
+            console.log('Data saved successfully:', savedDetails);
+            res.status(201).json({ message: 'Interview details saved successfully!' });
+        } else {
+            console.log('Data not saved.');
+            res.status(500).json({ error: 'Error saving interview details' });
+        }
     } catch (err) {
         res.status(500).send('Error'); // Send error response if something fails
     }
